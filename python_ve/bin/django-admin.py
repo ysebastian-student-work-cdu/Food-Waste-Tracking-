ecf1ce545120379f10c9ba54d63edf3771bfc43a
Matt <5d3si7ps@inboxe.anonaddy.com>
2023-03-29 14:17:34 +1100

    Fixed: Initialisation of project and the VE.

---
+++ python_ve/bin/django-admin.py
@@ -0,0 +1,21 @@
+#!/Users/study/Library/CloudStorage/OneDrive-CharlesDarwinUniversity/Bachelor of Computer Science & Master of Information Technology/1. Units/HIT237 Building Interactive Software/VSCode/HIT237 Building Interactive Software/Group Project/HIT237_S123_Group2/python_ve/bin/python
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
