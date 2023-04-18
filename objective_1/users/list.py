c69feb1c20108405a881a8e2e3a724ba539fcd77
Meherina <90877129+Meherina@users.noreply.github.com>
2023-04-18 15:59:57 +0930

    list.py added

---
+++ objective_1/users/list.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from . import views
+
+def home(request):
+    return render(request, 'users/index.html')
+
+def facts(request):
+    return render(request, 'users/facts.html')
+
+def reasons(request):
+    return render(request, 'users/reasons.html')
+
+def solutions(request):
+    return render(request, 'users/solutions.html')
+
+def benefits(request):
+    return render(request, 'users/benefits.html')
+
+def data_model(request):
+    return render(request, 'users/data_model.html')
+
+def resources(request):
+    return render(request, 'users/resources.html')
+
