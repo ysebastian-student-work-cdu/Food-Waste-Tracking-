7eb9035b899a7c4a3dd37bc9a216bf3b885972ff
Arsh <arshhasan68@gmail.com>
2023-04-19 15:10:33 +0930

    11

--- objective_1/users/urls.py
+++ objective_1/users/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index
+    path('', views.index, name='index'),
+    
     path('data_model', views.wastage, name='data_model'),
     path('list', views.list, name='list'),
     path('calculator', views.Calindex, name='calculator'),
     path('analyze', views.calculator, name='calculator_analyzed'),
     path('items/<id>', views.detail, name='items'),
-    # invalid path must stay at bottom of list
     path('<invalid>', views.invalid, name='invalid'),
 ]
