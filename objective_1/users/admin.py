130dffece2651896193f967627050c0c106de379
Meherina <90877129+Meherina@users.noreply.github.com>
2023-05-16 09:11:36 +0930

    Update admin.py

--- objective_1/users/admin.py
+++ objective_1/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import organisation
+from .models import Users
+from .models import WasteCollection
 
-# Register your models here.
+admin.site.register(organisation)
+admin.site.register(Users)
+admin.site.register(WasteCollection)
+
+
