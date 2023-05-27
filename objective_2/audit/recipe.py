9837c37fbbaea89a658621da154da23575e46604
Arsh <arshhasan68@gmail.com>
2023-05-27 15:27:15 +0930

    Try ^audit/add_food/, API works

--- objective_2/audit/recipe.py
+++ objective_2/audit/recipe.py
@@ -1,53 +1,36 @@
 import requests
 import json
-'''import requests
 
-url = 'https://api.openai.com/v1/chat/completions'
-headers = {'Authorization': 'Bearer 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','Content-Type': 'application/json'}
+class Recipe:
+    def generate_recipe(item1, item2, item3):
+        # Concatenate the food item names
+        food_items = f"{item1}, {item2}, {item3}"
 
-body = """{
-  "model": "gpt-3.5-turbo",
-  "messages": [{"role": "user", "content": f"Generate a recipe using the following ingredients: {food_items}"}]
-}"""
+        # Define the prompt for the ChatGPT API request
+        prompt = f"Generate a recipe using the following ingredients: {food_items}"
 
-req = requests.post(url, headers=headers, data=body)
-
-print(req.status_code)
-print(req.headers)
-print(req.text)'''
-
-def generate_recipe(item1, item2, item3):
-    # Concatenate the food item names
-    food_items = f"{item1}, {item2}, {item3}"
-
-    # Define the prompt for the ChatGPT API request
-    prompt = f"Generate a recipe using the following ingredients: {food_items}"
-
-    # Set your OpenAI API key
-    api_key = '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'
-    
-    # Make the API request to OpenAI ChatGPT3.5 Turbo
-    response = requests.post(
-        'https://api.openai.com/v1/chat/completions',
-        headers={
-            'Authorization': f'Bearer {api_key}',
-            'Content-Type': 'application/json'
-        },
-        json={
-            "model": "gpt-3.5-turbo",
-            "messages": [{"role": "user", "content": prompt}],
-            "temperature": 0.7
-             # Adjust the temperature for varied output
-        }
-    )
-    
-    print(response)
-
-    # Retrieve the generated recipe from the API response
-    if response.status_code == 200:
-        data = response.json()
-        return data['choices'][0]['message']['content']
-    else:
-        return 'API call failed'
-x= generate_recipe("chicken", "rice", "broccoli")
-print(x)
+        # Set your OpenAI API key
+        api_key = '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'
+        
+        # Make the API request to OpenAI ChatGPT3.5 Turbo
+        response = requests.post(
+            'https://api.openai.com/v1/chat/completions',
+            headers={
+                'Authorization': f'Bearer {api_key}',
+                'Content-Type': 'application/json'
+            },
+            json={
+                "model": "gpt-3.5-turbo",
+                "messages": [{"role": "user", "content": prompt}],
+                "temperature": 0.7
+                # Adjust the temperature for varied output
+            }
+        )
+        
+        # Retrieve the generated recipe from the API response
+        if response.status_code == 200:
+            data = response.json()
+            recipe= data['choices'][0]['message']['content']
+            return recipe
+        else:
+            return 'API call failed'
