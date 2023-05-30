4281c07ddfd6c34435e57ea9cf8124f5f5f371f8
JJ-Byte1 <s356169@students.cdu.edu.au>
2023-05-30 21:56:08 +0930

    added donation read to audit homepage

--- objective_2/audit/urls.py
+++ objective_2/audit/urls.py
@@ -21,8 +21,9 @@ urlpatterns = [
     path('items/<int:waste_entry_id>/', views.waste_items, name='items'),
     
     path('donate', views.donate, name='donate'),
-   path('donate/past', views.donate_read, name = 'past_donations'),
-    path('donate/update', views.donate_update, name = 'update_donations'),
+   path('donate/past', views.donate_read, name = 'donate_read'),
+    path('donate/update', views.donate_update, name = 'donate_update'),
+    path('donate/delete', views.donate_delete, name = 'donate_delete'),
     path('', views.accounts, name='accounts'),
     path('add_food/',views.add_food,name='add_food'),
     path('recipefoodsave.html/',views.savefood,name='savefoodss'),
