4f788ba9cdaa08b7bb5a28174d9e5786c853c8a5
Arsh <arshhasan68@gmail.com>
2023-05-31 12:31:55 +0930

    List transferred

--- objective_2/audit/views.py
+++ objective_2/audit/views.py
@@ -120,48 +120,8 @@ def complete_payment(request, payment_id):
 
     return JsonResponse({'message': 'Payment completed successfully.'})
 
-'''
-Adds a new user to the user table
-'''
-def user_create(request):
-	return 0
-	
-'''
-Updates a user's password in the user table
-'''
-def user_update(request):
-	return 0
-
-
-'''
-Deletes a user from the user table
-'''
-def user_delete(request):
-	return 0
-
-
-'''
-Deletes a user from the user table
-'''
-def user_delete(request):
-	return 0
-
-'''
-	Logs new entry into food audit table
-'''
-def log_create(request):
-	return 0
-
-
-'''
-Displays all entries into food audit table by specific user
-'''
-def log_read(request):
-	return 0
-
-'''
-	Adds a new donation by user to a food waste org to donation table
-'''
+def list(request):
+    return render(request, 'audit/AuditHomepage.html')
 
 def donate(request):
     error = 2
--- objective_2/users/urls.py
+++ objective_2/users/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('data_model', views.wastage, name='data_model'),
-    path('list', views.list, name='list'),
     path('calculator', views.Calindex, name='calculator'),
     path('analyze', views.calculator, name='calculator_analyzed'),
     path('items/<id>', views.detail, name='items'),
