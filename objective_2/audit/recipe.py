b563ef1e7cf6f7735fbb63c3313bb7337b31b306
Arsh <arshhasan68@gmail.com>
2023-05-31 23:24:04 +0930

    Removed the API key

--- objective_2/audit/recipe.py
+++ objective_2/audit/recipe.py
@@ -10,7 +10,7 @@ class Recipe:
         prompt = f"Generate a recipe using the following ingredients: {food_items}"
 
         # Set your OpenAI API key
-        api_key = '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'
+        api_key = ''
         
         # Make the API request to OpenAI ChatGPT3.5 Turbo
         response = requests.post(
